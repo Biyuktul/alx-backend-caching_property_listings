b71780127430b85a469ac762ca35b1686547e3c9
Yonatan <yonatanaddis0257@gmail.com>
2025-08-08 20:59:38 +0300

    adding files

--- alx_backend_caching_property_listings/properties/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PropertyViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'properties', PropertyViewSet, basename='property')
-
-urlpatterns = router.urls
